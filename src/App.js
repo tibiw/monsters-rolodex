61e024c4e2c28ec11b728534e590f3e277bde077
tibiw <tibor.waxmann@gmail.com>
2020-11-22 21:06:17 +0200

    added title font and background

--- src/App.js
+++ src/App.js
@@ -14,12 +14,18 @@ class App extends React.Component {
       monsters: [],
       searchField: "",
     };
+
+    //this.handleChange = this.handleChange.bind(this);
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
       response.json().then((users) => this.setState({ monsters: users }))
     );
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
   }
 
   render() {
@@ -30,9 +36,10 @@ class App extends React.Component {
 
     return (
       <div className="App">
+      <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
